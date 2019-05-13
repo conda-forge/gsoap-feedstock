1dde247e50ade2c45fb8bd598e676f32245e94f8
Chris Burr <christopher.burr@cern.ch>
2019-05-13 15:00:28 +0200

    Build 2.7.13

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,13 +1,23 @@
 #!/usr/bin/env bash
 set -eu
 
+# These are needed to fix building with C++ 11 support
+sed -i 's@"FMT"@" FMT "@g' gsoap/Makefile.in
+sed -i 's@"VERSION"@" VERSION "@g' gsoap/wsdl/wsdl2h.cpp
+sed -i 's@"VERSION"@" VERSION "@g' gsoap/wsdl/service.cpp
+
+# patches change autoconf and automake files, so we must reconfigure
+autoreconf --install --force
+
+# enable IPv6 support
+sed 's/Cflags:/& -DWITH_IPV6/' -i gsoap*.pc.in
+CFLAGS="-DWITH_IPV6 ${CFLAGS}"
+CXXFLAGS="-DWITH_IPV6 ${CXXFLAGS}"
+
 ./configure \
     --prefix="${PREFIX}" \
-    --with-openssl="${PREFIX}/" \
-    --with-zlib="${PREFIX}/" \
-    --enable-ipv6
+    --disable-shared
 
 # Using multiple cores fails so explicitly use -j1
 make -j1
-# make -j${CPU_COUNT}
 make install
